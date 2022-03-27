7721ccbbb1147654f944b96b8afd872c60121111
Bruno <bruno.sdsoares@gmail.com>
2022-03-27 18:52:19 -0300

    adicionando carrossel

---
+++ src/javascript/script.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+    const slickOption = {
+        autoplay: true,
+        dots: false,
+        prevArrow: '<button type="button" class="slick-prev slider__prev-arrow" >Previous</button>;',
+        nextArrow: '<button type="button" class="slick-next slider__next-arrow" >Previous</button>;',
+    };  
+    $('.slider').slick(slickOption);
+});
