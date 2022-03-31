4c4026b2575d9ed365116d23f6205226ff1afd3f
Bruno <bruno.sdsoares@gmail.com>
2022-03-31 00:09:01 -0300

    adicionando responsividade

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -2,23 +2,24 @@ $(document).ready(() => {
     const slickOption = {
         autoplay: true,
         dots: false,
-        prevArrow: '<button type="button" class="slick-prev slider__prev-arrow" >Previous</button>;',
-        nextArrow: '<button type="button" class="slick-next slider__next-arrow" >Previous</button>;',
+        prevArrow: '<button type="button" class="slick-prev slider__prev-arrow">Previous</button>;',
+        nextArrow: '<button type="button" class="slick-next slider__next-arrow">Previous</button>;',
     };  
     $('.slider').slick(slickOption);
-
-    // $(".footer__form-button").on("click", () => {
-    //     const email $("#email").val();
-    //     Email.send({
-    //         Host: "smtp.yourisp.com",
-    //         Username: "username",
-    //         Password: "password",
-    //         To: 'them@website.com',
-    //         From: email,
-    //         Subject: "This is the subject",
-    //         Body: "And this is the body"
-    //     }).then(
-    //         message => alert(message)
-    //     );
-    // });
 });
+
+
+    /* $(".footer__form-button").on("click", () => {
+         const email $("#email").val();
+         Email.send({
+             Host: "smtp.yourisp.com",
+             Username: "username",
+             Password: "password",
+             To: 'them@website.com',
+             From: email,
+             Subject: "This is the subject",
+             Body: "And this is the body"
+         }).then(
+             message => alert(message)
+         );  style=display: none
+     });*/
